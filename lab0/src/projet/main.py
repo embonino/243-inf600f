d822f00f1a58c94804bf481c012b4ed08bdb8d1f
comtois-etienne <comtois.etienne@outlook.com>
2024-09-06 16:46:07 -0400

    Update main.py

--- lab0/src/projet/main.py
+++ lab0/src/projet/main.py
@@ -17,6 +17,11 @@ class Person:
         self.name = name
         self.age = age
 
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Person):
+            return False
+        return self.name == value.name and self.age == value.age
+
     def get_name(self):
         return self.name
     
